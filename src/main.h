823b8eb3e99839ec6332f69aa82b4cab322231c7
ivanTorda <ivan.torda@gmail.com>
2016-10-30 20:59:19 +0100

    pridany switch mode, ak sa posle m odosle adc hodnotu, po dalsom poslani odosle realne napatie.

--- src/main.h
+++ src/main.h
@@ -36,8 +36,6 @@
 /* Exported constants --------------------------------------------------------*/
 /* Exported macro ------------------------------------------------------------*/
 /* Exported functions ------------------------------------------------------- */
-void TimingDelay_Decrement(void);
-void Delay(__IO uint32_t nTime);
 #endif /* __MAIN_H */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
